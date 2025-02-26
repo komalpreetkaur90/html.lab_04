e30fb7c5288bc428303e0b77d488fe69f2968fc2
Komalpreet kaur <kkaur209@academic.rrc.ca>
2025-02-25 21:22:42 -0600

    Added Display Validation Messages

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Form Submission Event Listener
 const form = document.getElementById("EventRegistrationForm");
 const submit = form.elements["Register"].value;
 
@@ -5,7 +6,16 @@ document.getElementById("Register").addEventListener("change", (event) => {
     event.preventDefault();
 });
 
+// Validate the Number Input
 function validateNumbericCode(code) {
     const tickets = /^\d{5}$/;
     return tickets.test(code);
+}
+
+// Display Validation Messages
+function showNumbericCodeError(numbericCode, message) {
+    const errorDisplay = document.createElement("span");
+    errorDisplay.innerText = message;
+    errorDisplay.className = "error-messge";
+    numbericCode.parentElement.appendChild(errorDisplay);
 }
