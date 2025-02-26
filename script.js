8695352981851e16d883f0a3a2bc267cb3a72fb5
Komalpreet kaur <kkaur209@academic.rrc.ca>
2025-02-26 16:13:20 -0600

    Done validation for additional inputs

--- script.js
+++ script.js
@@ -1,21 +1,64 @@
+console.log("Script loaded successfully.");
+
 // Form Submission Event Listener
-const form = document.getElementById("EventRegistrationForm");
-const submit = form.elements["Register"].value;
-
-document.getElementById("Register").addEventListener("change", (event) => {
+const form = document.getElementById("eventRegistrationForm");
+console.log("Form element loaded.");
+form.addEventListener("submit", (event) => {
+    console.log("Form submission attempted.");
     event.preventDefault();
-});
+    });
 
-// Validate the Number Input
-function validateNumbericCode(code) {
-    const tickets = /^\d{5}$/;
-    return tickets.test(code);
-}
+    // Validate Username
+    const username = document.getElementById("username").value;
+    if (username === "") {
+        alert("Username must be filled out.");
+        return;
+    }
 
-// Display Validation Messages
-function showNumbericCodeError(numbericCode, message) {
-    const errorDisplay = document.createElement("span");
-    errorDisplay.innerText = message;
-    errorDisplay.className = "error-messge";
-    numbericCode.parentElement.appendChild(errorDisplay);
-}
+    // Validtation for Ticket number
+    const number = document.getElementById("tickets")
+    const errorbox = document.createElement("span");
+    tickets.insertAdjacentElement("afterend", errorbox);
+    errorbox.style.color ="Blue";
+
+    const ticketValue = tickets.value.trim();  
+    function validatePositiveNumber(number) {
+    if (number >= 1){
+        return true
+    }
+    return false;
+    }
+    const ticketNumber = Number(ticketValue);
+    if (!validatePositiveNumber(ticketNumber)) {
+        errorbox.textContent = "Enter positive number of tickets.";
+        console.error("Enter a positive value.");
+        return;
+    }
+
+    // validate Phone Number
+    const phone = document.getElementById("phone").value;
+    function validateNumericCode(tel) {
+        const pattern = /^\d{10}$/;
+        alert("Phone number must be 10 digits.");
+        return pattern.test(tel);
+    }
+    console.log("Phone number validated successfully.");
+
+    //  validatation for checkbox
+    document.addEventListener("DOMContentLoaded", () => {
+        const checkboxes = document.querySelectorAll('input[name= "eventType"]');
+        console.log("Checkboxes selected:", checkboxes);
+
+        checkboxes.forEach((checkbox) => {
+            checkbox.addEventListener("change", function () {
+                checkboxes.forEach((cb) => {
+                    if (cb !== this) cb.checked = false;
+                })
+            });
+        });
+    })
+
+// Usage in Validation
+if (username == "") {
+    showInputError (document. getElementById ("username"), "Username is required");
+    }
